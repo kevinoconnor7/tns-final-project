7af812231c9834d9c2b55dca734529eba8038348
Kevin O'Connor <kevin.oconnor7@gmail.com>
2014-12-04 16:12:50 -0500

    Move a bunch of code out to seperate files

---
+++ basic_player.py
@@ -0,0 +1,186 @@
+# This notebook is for working out the details of the overall model we're going to have. The actual model is
+# completely abstracted, and serves mostly as a moderator for the AI 'players' submitted to it.
+
+import numpy as np
+
+class basic_player:
+    def initialize(self, faction, n_players, idx):
+        self.faction = faction # True for resistance, False for spy
+        self.idx = idx
+        self.n_players = n_players
+        self.spies = []
+        self.opinions = self.init_opinions(n_players)
+        self.weights = self.init_weights(n_players)
+        self.game_history = []
+        self.max_opinion_diff = .5
+        self.history = []
+
+    def init_opinions(self, n):
+        """ Figures out the initial opinions of each n players. Base player does uniformly 0.5 """
+        opin = []
+        if len(self.spies):
+            opin = [1.] * n
+            for idx in self.spies:
+                opin[idx] = 0.
+        else:
+            for i in range(n):
+                opin.append(np.random.rand()*.2+.4)
+
+        opin[self.idx] = float(self.faction)
+
+        return opin
+
+    def init_weights(self, n):
+        """ This function initializes the weights for each player in each opinion network.
+        The base player takes care to initialize the weights for their own network to 0 for everyone but itself,
+        so it cannot be convinced that it is a spy when it is not a spy. All other networks will have 1/degree
+        weights. Weight lists will be recalculated between consensus rounds. """
+        weight_lists = []
+        for p in range(n):
+            if p != self.idx:
+                weight_lists.append([1/n]*n)
+            else:
+                weight_lists.append([0 if i!=self.idx else 1 for i in range(n)])
+        return weight_lists
+
+
+    def calc_real_opinion(self, neighbor_input):
+        """ This function is called iteratively during the 'discussion' phase. neighbor_input is going to be a list of
+        input vectors and the (perceived) opinions given for each player """
+        # The base player is going to use the bounded confidence model if resistance
+        # if it's a spy, it should not update its own opinions (as it knows the truth) but
+        # instead hold a fake opinion that mirrors its resistance strategy
+
+        # Save what we hear into history
+        self.history.append(neighbor_input)
+
+        all_opinions = np.array(neighbor_input).T
+        self.weights = []
+        for i in range(len(all_opinions)):
+            if i == self.idx:
+                self.weights.append([0 if i!=self.idx else 1 for z in range(len(all_opinions))])
+                continue
+
+            ith_weights = []
+            within_range = 0
+            for j in range(len(all_opinions[i])):
+                if abs(self.opinions[j] - all_opinions[i][j]) <= self.max_opinion_diff:
+                    within_range += 1
+
+            for j in range(len(all_opinions[i])):
+                if abs(self.opinions[j] - all_opinions[i][j]) <= self.max_opinion_diff:
+                    ith_weights.append(1./within_range)
+                else:
+                    ith_weights.append(0)
+            self.weights.append(ith_weights)
+
+        new_opinions = np.array(self.weights)*all_opinions
+        self.opinions = new_opinions.T[self.idx]
+
+    def calc_percv_opinion(self):
+        """ Based on self.opinions, other player's (perceived) opinions and the game history,
+        come up with an opinion that the player wants others players to perceive. """
+
+        # The base player can simply return their actual opinion if they're resistance, or the
+        # aforementioned fake opinion if they're a spy
+        if self.faction:
+            return self.opinions
+        else:
+            opin = np.random.rand(len(self.opinions))
+            opin[self.idx] = 1
+            return opin
+            #return [0.5]*len(self.opinions)
+        pass
+
+    def team_select(self, mission_details):
+        """ This is called iff the player is called on to select other players for a mission. It is given the
+        mission details (a tuple of (req_players,n_spies_to_fail)) and outputs a selection of players """
+        #TODO implement
+        # The base player can disregard all history and just select the players it believes least likely to be
+        # spies (if it's resistance). If it's a spy, the player can do the same and choose as many spies as required
+        # to fail.
+        req_players, n_spies_to_fail = mission_details
+        team = None
+        if self.faction:
+            team = [i[0] for i in sorted(enumerate(self.opinions), key=lambda x:x[1], reverse=True)[:req_players]]
+        else:
+            # TODO Spies should already know who is a spy so they should select
+            # the spy with the lowest detection
+            team = [self.idx]
+            most_trusted = [i[0] for i in sorted(enumerate(self.opinions), key=lambda x:x[1], reverse=True)]
+            most_trusted.remove(self.idx)
+            team += most_trusted[:(req_players-1)]
+        return team
+
+    def vote_on_select(self, team, mission_details, last_vote=False):
+        """ This is called when a leader has selected a team. The player uses the mission details and the
+        chosen players, and returns either True (for yes) or False."""
+        #TODO implement
+        # The base player should just vote no if it believes with a high certainty there is a spy on the team
+        # (or yes otherwise) if it's resistance and flip that for a spy.
+
+        if last_vote:
+            return True
+
+        req_players, n_spies_to_fail = mission_details
+
+        n_suspected_spies = 0
+        n_suspected_resistance = 0
+
+        median_opinion = np.median(self.opinions)
+
+        for i in team:
+            if self.opinions[i] < median_opinion:
+                n_suspected_spies += 1
+            else:
+                n_suspected_resistance += 1
+
+        if self.faction:
+            return n_suspected_spies == 0
+        else:
+            return n_suspected_spies >= n_spies_to_fail
+
+    def set_spies(self, spies):
+        self.spies = spies
+        self.init_opinions(self.n_players)
+
+    def execute_mission(self):
+        """ This is called when a player is selected for a mission. A resistance player will always pass
+        (return True), however a spy may choose not to fail the mission """
+        #TODO implement
+        # The resistance part here is pretty trivial
+        # If you're a spy, then you can make a choice, but the basic player will just return True for resistance
+        # and False for a spy
+        return self.faction
+
+    def recalc_opinion(self, current_leader, phase=None, extra=None):
+        """ This function recalculates opinions for the phases: team_sel, votes, post_mission """
+        if not self.faction:
+            return
+        #TODO
+        if phase=="team_sel":
+            #Decrease opinion of leader if you suspect mission contains a spy
+            for player in extra:
+                if player == self.idx:
+                    self.opinions[current_leader] = (self.opinions[current_leader]*1.2)%1
+                if player in sorted(self.opinions)[:len(extra)]:
+                    self.opinions[current_leader] *= 0.8
+        elif phase=="vote":
+            #Increase/Decrease opionion of everyone based on whether they voted with/against you
+            for player in range(len(extra)):
+                if extra[player] == extra[self.idx]:
+                    self.opinions[player] = (self.opinions[player]*1.2)%1
+                else:
+                    self.opinions[player] *= 0.8
+        elif phase=="post-mission":
+            #Increase/Decrease opinion of everyone on team (and leader) if mission passed/failed.
+            if extra[0]:
+                for player in extra[1]:
+                    self.opinions[player.idx] *= 1.4
+                if current_leader not in extra[1]:
+                    self.opinions[current_leader.idx] *= 1.4
+            else:
+                for player in extra[1]:
+                    self.opinions[player.idx] *= 0.95
+                if current_leader not in extra[1]:
+                    self.opinions[current_leader.idx] *= 0.8
---
+++ game_mod.py
@@ -0,0 +1,136 @@
+import numpy as np
+
+# Framework: the game will proceed in phases
+# Initialization
+# For each mission:
+#  until vote is passed:
+#  discussion
+#  team selection
+#  recalculate_opinion
+#  discussion
+#  vote
+#  recalculate_opinion
+#  if the vote failed, we go back
+#  if the vote failed for the 5th time, fail and start a new mission
+#  if the vote passed
+#   execute_mission
+#   recalculate_opinion
+class game_moderator:
+    def __init__(self, players):
+        """ Give the game moderator a list of instantiated players """
+        self.players = []
+        self.n_players = len(players)
+        self.n_spies = 1 + (self.n_players-1) // 3 # 5 <= Num_players <= 10
+        # randomly choose spies by getting the probability that any player is a spy
+        is_res = [True] * (self.n_players - self.n_spies) + [False] * self.n_spies
+        np.random.shuffle(is_res)
+        #prob_spy = self.n_spies / self.n_players
+        #spies_left = self.n_spies
+        # reorder players to prevent bias of spies being assigned to earlier players
+        np.random.shuffle(players)
+        for idx, player in enumerate(players):
+            #if ((np.random.rand() < prob_spy) and (spies_left > 0)) or self.n_players-idx <= spies_left:
+            #    is_res = False
+            #    spies_left -= 1
+            player.initialize(is_res[idx], self.n_players, idx)
+            self.players.append(player)
+        self.missions = self.get_missions()
+        self.current_leader = 0
+
+        spies = [player.idx for player in self.players if not player.faction]
+        for player in self.players:
+            if not player.faction:
+                player.set_spies(spies)
+
+    def get_missions(self):
+        """ Returns a list of 5 missions according to the game rules for the given number of players """
+        mission_rules = {
+        5:[(2,1),(3,1),(2,1),(3,1),(3,1)],
+        6:[(2,1),(3,1),(4,1),(3,1),(4,1)],
+        7:[(2,1),(3,1),(3,1),(4,2),(4,1)],
+        8:[(3,1),(4,1),(4,1),(5,2),(5,1)],
+        9:[(3,1),(4,1),(4,1),(5,2),(5,1)],
+        10:[(3,1),(4,1),(4,1),(5,2),(5,1)]
+        }
+        try:
+            return mission_rules[self.n_players]
+        except KeyError as ke:
+            print("Invalid number of players")
+            raise
+
+    def run_mission(self, cur_mission):
+        """ This will actually run the mission index specified """
+        n_votes = 0
+        prop_team = []
+        while (True):
+            print("Voting round %d" % n_votes)
+            if n_votes == 6: ## vote limit, fail the mission
+                return False
+            self.discuss_opinions()
+            prop_team = self.team_select(self.current_leader, cur_mission)
+            for idx in prop_team:
+                print("%d: Is resistance? %s" % (idx, self.players[idx].faction))
+            for player in self.players:
+                player.cur_leader = self.current_leader
+                player.prop_team = prop_team
+                player.cur_mission = cur_mission
+                player.vote_num = n_votes
+            self.recalc_opinions(phase="team_sel", extra=prop_team)
+            self.discuss_opinions()
+            vote_passed, votes = self.get_votes(prop_team, cur_mission, n_votes == 5)
+            self.recalc_opinions(phase="vote",extra=votes)
+            if vote_passed:
+                break
+            else:
+                self.current_leader += 1
+                self.current_leader %= self.n_players
+                n_votes += 1
+        # Once we're out of that loop, we can execute the mission and have players recalculate their opinions
+        n_pass, n_fail = self.execute_mission(prop_team)
+
+        # The mission succeeds if less than the number of spies needed to fail the mission
+        # had failed the mission
+        mission_result = n_fail < cur_mission[1]
+
+        self.recalc_opinions(phase="post_mission",extra=(mission_result, prop_team))
+        self.current_leader = (self.current_leader+1) % self.n_players
+        return mission_result
+
+    def discuss_opinions(self, n_iter=None):
+        """ This function takes each player's perceived opinions and broadcasts them to every player for
+        in a random order. If n_iter is not specified, it will try to run until convergence """
+        opinions = []
+        for player in self.players:
+            opinions.append(player.calc_percv_opinion())
+
+        for player in self.players:
+            player.calc_real_opinion(opinions)
+
+    def team_select(self, current_leader, current_mission):
+        """ This function just calls the current leader's team_select function, which returns the proposed team"""
+        return self.players[current_leader].team_select(current_mission)
+
+    def recalc_opinions(self, phase=None, extra=None):
+        """ Goes through each player and calls their recalc_opinions function """
+        for player in self.players:
+            player.recalc_opinion(self.current_leader, phase, extra)
+
+    def get_votes(self, prop_team, cur_mission, last_vote=False):
+        """ Goes through each player to get their vote on the proposed team """
+        votes = []
+        for player in self.players:
+            votes.append(player.vote_on_select(prop_team, cur_mission, last_vote))
+
+        vote_passed = len([x for x in votes if x]) > self.n_players/2
+        return vote_passed, votes
+
+    def execute_mission(self, prop_team):
+        """ Goes through the proposed team and calls their execute_mission function, then
+        collects the responses """
+        n_pass, n_fail = 0, 0
+        for player in prop_team:
+            if self.players[player].execute_mission():
+                n_pass += 1
+            else:
+                n_fail += 1
+        return n_pass, n_fail
